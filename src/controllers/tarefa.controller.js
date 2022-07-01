3f18a34f5118891e07494824641a4f2f86d86a59
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-07-01 17:57:21 -0300

    update

--- src/controllers/tarefa.controller.js
+++ src/controllers/tarefa.controller.js
@@ -1,31 +1,80 @@
 const tarefasService = require('../services/tarefa.service');
 
 const findAllTarefasController = (req, res) => {
-  const allTarefas = tarefasService.findTarefaService();
-  res.send(allTarefas);
+  const Tarefas = tarefasService.findTarefaService();
+  if (Tarefas.length == 0) {
+    res.status(404).json({ message: 'Não existem tarefas cadastrada' });
+  }
+  res.send(Tarefas);
 };
 const findByIdTarefaController = (req, res) => {
   const parametroId = +req.params.id;
+
+  if(!parametroId){
+    return res.status(400).send({message:'ID invalido'});
+  }
+
   const escolhaTarefa = tarefasService.findTarefaByIdService(parametroId);
+  if(!escolhaTarefa){
+    return res.status(404).send({message:'Tarefa invalida'});
+  }
   res.send(escolhaTarefa);
 };
 
 const createTarefaController = (req, res) => {
   const tarefa = req.body;
+
+if(!tarefa || !tarefa.tema || !tarefa.tempo){
+  
+  if(!tarefa.tema && !tarefa.tempo){
+    return res.status(400).send({message:"Por favor preencha todos os campos"})
+  }
+
+  if(!tarefa.tema){
+    return res.status(400).send ({message:"Porfavor insira uma tema"})
+  }
+
+  if(!tarefa.tempo){
+    return res.status(400).send ({message:"Porfavor insira o tempo"} )
+  }
+
+}
+
   const newtarefa = tarefasService.createTarefaService(tarefa);
   res.send(newtarefa);
 };
 
 const updateTarefaController = (req, res) => {
-  const idParam = +req.params.id;
+  const idTarefa = +req.params.id;
+  if(!idTarefa){
+    return res.status(400).send({message:'ID invalido'});
+  }
   const tarefaEdit = req.body;
-  const updateTare = tarefasService.updateTarefaService(idParam, tarefaEdit);
+  if(!tarefaEdit || !tarefaEdit.tema || !tarefaEdit.tempo){
+  
+    if(!tarefaEdit.tema && !tarefaEdit.tempo){
+      return res.status(400).send({message:"Por favor preencha todos os campos"})
+    }
+  
+    if(!tarefaEdit.tema){
+      return res.status(400).send ({message:"Porfavor insira uma tema"})
+    }
+  
+    if(!tarefaEdit.tempo){
+      return res.status(400).send ({message:"Porfavor insira o tempo"} )
+    }
+  }
+  
+  const updateTare = tarefasService.updateTarefaService(idTarefa, tarefaEdit);
   res.send(updateTare);
 };
 
 const deleteTarefaController = (req, res) => {
-  const idParam = +req.params.id;
-  tarefasService.deleteTarefaService(idParam);
+  const idTarefa = +req.params.id;
+  if(!idTarefa){
+    return res.status(400).send({message:'ID invalido'});
+  }
+  tarefasService.deleteTarefaService(idTarefa);
   res.send({ message: 'Tarefa Deletada com sucesso!!' });
 };
 
